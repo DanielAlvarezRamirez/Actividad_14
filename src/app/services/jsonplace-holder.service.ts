0cd1161d1fe5b955f0869c22364188df57db7917
DanielAlvarezRamirez <d.a.alvarez.ramirez@gmail.com>
2022-06-27 00:40:56 -0500

    Version 3.0.0

--- src/app/services/jsonplace-holder.service.ts
+++ src/app/services/jsonplace-holder.service.ts
@@ -23,4 +23,26 @@ export class JSONPlaceHolderService {
     return this.http.get<any>(this.url + "/users");
   } 
 
+  createPost(title: string, body: string){
+    fetch(this.url + "/posts", {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: Math.floor(Math.random() * (999 - 100 + 1)) + 100,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then((json) => alert(json));
+  }
+
+  deletePost(id: string) {
+    fetch(this.url + "/posts/" + id, {
+      method: 'DELETE',
+    })
+    .then(() => alert('Post ' + id + ' deleted.'));
+  }
+
 }
