61bc9b43813b84ce826060afbfbe1e1a69848e17
DanielAlvarezRamirez <d.a.alvarez.ramirez@gmail.com>
2022-06-26 20:25:26 -0500

    Get de resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JSONPlaceHolderService } from './services/jsonplace-holder.service'; 
 
 @Component({
@@ -6,14 +6,22 @@ import { JSONPlaceHolderService } from './services/jsonplace-holder.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isPosts: boolean = true;
+  isPhotos: boolean = false;
+  isUsers: boolean = false;
   data: Array<any>;
 
   constructor(private JSONPlaceHolder:JSONPlaceHolderService){
     this.data = new Array<any>();
   }
 
+  ngOnInit(): void {
+    this.getPostsFromAPI();
+  }
+
   getPostsFromAPI(){
+    this.changeSelected('posts');
     this.JSONPlaceHolder.getPosts().subscribe((data) => {
       console.log(data);
       this.data = data;
@@ -21,6 +29,7 @@ export class AppComponent {
   }
 
   getPhotosFromAPI(){
+    this.changeSelected('photos');
     this.JSONPlaceHolder.getPhotos().subscribe((data) => {
       console.log(data);
       this.data = data;
@@ -28,9 +37,26 @@ export class AppComponent {
   }
 
   getUsersFromAPI(){
+    this.changeSelected('users');
     this.JSONPlaceHolder.getUsers().subscribe((data) => {
       console.log(data);
       this.data = data;
     })
+  }
+
+  changeSelected(selectedResource : string): void{
+    if(selectedResource === 'posts'){
+      this.isPosts = true;
+      this.isPhotos = false;
+      this.isUsers = false;
+    } else if(selectedResource === 'photos') {
+      this.isPosts = false;
+      this.isPhotos = true;
+      this.isUsers = false;
+    } else if(selectedResource === 'users'){
+      this.isPosts = false;
+      this.isPhotos = false;
+      this.isUsers = true;
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
